9f6eb07a90772f5ad627266d536b0343050b5768
Jared <60214343+jowens-self@users.noreply.github.com>
2022-10-01 00:26:34 -0500

    Rough Draft 1

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,35 @@
+package $org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void convertJsonToJavaClass(URL inputJsonUrl, File outputJavaClassDirectory, String packageName, String javaClassName)
+            throws IOException {
+        JCodeModel jcodeModel = new JCodeModel();
+
+        GenerationConfig config = new DefaultGenerationConfig() {
+            @Override
+            public boolean isGenerateBuilders() {
+                return true;
+            }
+
+            @Override
+            public SourceType getSourceType() {
+                return SourceType.JSON;
+            }
+        };
+
+        SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
+        mapper.generate(jcodeModel, javaClassName, packageName, inputJsonUrl);
+
+        jcodeModel.build(outputJavaClassDirectory);
+    }
+}
